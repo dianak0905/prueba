c1254db171b6ca0e395a68665aa946246dc3c075
s107e9 <s107e9@Sala107_09>
2012-05-09 06:44:06 -0500

    pruebita

---
+++ prueba/src/prueba/Prueba.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author s107e9
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("que sueño");
+    }
+}
